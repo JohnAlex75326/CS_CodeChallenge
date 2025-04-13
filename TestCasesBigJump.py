8860ce3ec175be8e7740a352d82cba097a885ad6
JohnAlex75326 <johnalexandersarmiento26@gmail.com>
2025-04-13 20:24:15 +0800

    April 13

--- TestCasesBigJump.py
+++ TestCasesBigJump.py
@@ -1,3 +1,11 @@
 def jumps(flagHeight, bigJump):
     bigJumpCount = flagHeight // bigJump
     remainingHeight = flagHeight % bigJump
+    # Total jumps = big jumps + all needed 1-jumps
+    return bigJumpCount + remainingHeight
+
+if __name__ == "__main__":
+    import sys
+    flagHeight = int(sys.stdin.readline())
+    bigJump = int(sys.stdin.readline())
+    print(jumps(flagHeight, bigJump))
