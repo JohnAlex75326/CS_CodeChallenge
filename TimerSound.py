a64b565a6b13827f3debdc5cd87591326949d653
JohnAlex75326 <johnalexandersarmiento26@gmail.com>
2025-08-11 08:41:07 +0800

    Aug 12

--- TimerSound.py
+++ TimerSound.py
@@ -22,3 +22,11 @@ def speak_message(message):
         else:
             os.system('printf"\\a"')
         
+def stopwatch():
+    minutes_passed = 0
+    print("Stopwatch started. Press Ctrl+C to stop")
+    try:
+        while True:
+            time.sleep(60)
+            minutes_passed +=1
+            message = f"{minutes_passed} minute {'s' if minutes_passed > 1 else ''}passed"
